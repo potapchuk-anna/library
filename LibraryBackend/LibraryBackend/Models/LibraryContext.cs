63e67b75b4fed78cc3f41fd782c9cceed058ad6a
Анна Потапчук <potapchuk@dlit.dp.ua>
2022-08-05 14:08:15 +0300

    seeded data was added

--- LibraryBackend/LibraryBackend/Models/LibraryContext.cs
+++ LibraryBackend/LibraryBackend/Models/LibraryContext.cs
@@ -21,9 +21,15 @@ namespace LibraryBackend.Models
                 b.HasData(
                 new Book { Id = 1, Title = "Harry Potter 1", Author = "Joanna Rowling", Content = "Smth", Cover = "path1", Genre = "fantasy" },
                 new Book { Id = 2, Title = "Harry Potter 3", Author = "AJoanna Rowling", Content = "Smth", Cover = "path1", Genre = "fantasy" },
-                new Book { Id = 3, Title = "Harry Potter 2", Author = "BJoanna Rowling", Content = "Smth", Cover = "path1", Genre = "horror" });
-            }
-             );
+                new Book { Id = 3, Title = "Harry Potter 4", Author = "BJoanna Rowling", Content = "Smth", Cover = "path1", Genre = "horror" },
+                new Book { Id = 4, Title = "Harry Potter 5", Author = "BJoanna Rowling", Content = "Smth", Cover = "path1", Genre = "horror" },
+                new Book { Id = 5, Title = "Harry Potter 6", Author = "BJoanna Rowling", Content = "Smth", Cover = "path1", Genre = "horror" },
+                new Book { Id = 6, Title = "Harry Potter 7", Author = "BJoanna Rowling", Content = "Smth", Cover = "path1", Genre = "horror" },
+                new Book { Id = 7, Title = "Else", Author = "BJoanna Rowling", Content = "Smth", Cover = "path1", Genre = "horror" },
+                new Book { Id = 8, Title = "Anna", Author = "BJoanna Rowling", Content = "Smth", Cover = "path1", Genre = "horror" },
+                new Book { Id = 9, Title = "Cat", Author = "BJoanna Rowling", Content = "Smth", Cover = "path1", Genre = "horror" },
+                new Book { Id = 10, Title = "Dog", Author = "BJoanna Rowling", Content = "Smth", Cover = "path1", Genre = "horror" });
+            });
             modelBuilder.Entity<Rating>().HasData(
                 new Rating { Id = 1, BookId = 1, Score = 5 },
                 new Rating { Id = 2, BookId = 2, Score = 4 },
@@ -41,7 +47,15 @@ namespace LibraryBackend.Models
                 new Review { Id = 5, BookId = 1, Message = "Bad", Reviewer = "Anton" },
                 new Review { Id = 6, BookId = 3, Message = "Good", Reviewer = "Anton" },
                 new Review { Id = 7, BookId = 3, Message = "Bad", Reviewer = "Anton" },
-                new Review { Id = 8, BookId = 1, Message = "Good", Reviewer = "Anton" }
+                new Review { Id = 8, BookId = 1, Message = "Good", Reviewer = "Anton" },
+                new Review { Id = 9, BookId = 1, Message = "Good", Reviewer = "Anton" },
+                new Review { Id = 10, BookId = 1, Message = "Good", Reviewer = "Anton" },
+                new Review { Id = 11, BookId = 1, Message = "Good", Reviewer = "Anton" },
+                new Review { Id = 12, BookId = 1, Message = "Good", Reviewer = "Anton" },
+                new Review { Id = 13, BookId = 1, Message = "Good", Reviewer = "Anton" },
+                new Review { Id = 14, BookId = 1, Message = "Good", Reviewer = "Anton" },
+                new Review { Id = 15, BookId = 1, Message = "Good", Reviewer = "Anton" },
+                new Review { Id = 16, BookId = 1, Message = "Good", Reviewer = "Anton" }
                 );
         }
     }
